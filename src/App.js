1c66d32e950f5ff026582fd102949500f6c3c344
Rob Gomez <robgomez.sir@gmail.com>
2025-08-23 03:34:06 -0300

    Inicialização - aplicação React simples

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SistemaProposito from './components/SistemaProposito';
+
+function App() {
+  return <SistemaProposito />;
+}
+
+export default App;
---
+++ src/components/SistemaProposito.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SistemaProposito = () => {
+  return (
+    <div style={{
+      width: '100vw',
+      height: '100vh',
+      backgroundColor: '#FF0000',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: 'white',
+      fontSize: '48px',
+      fontWeight: 'bold',
+      textAlign: 'center'
+    }}>
+      FUNCIONANDO!
+    </div>
+  );
+};
+
+export default SistemaProposito;
